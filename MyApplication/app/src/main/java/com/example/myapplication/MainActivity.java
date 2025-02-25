75525239b6f70f4a3360ea1683cd3505364ac113
Anca Maria <bcm15102002@gmail.com>
2025-02-25 11:45:01 +0200

    Am refacut proiectul si am adaugat functionalitatile pentru lab2

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -8,6 +8,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -19,6 +24,34 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        EditText editText1 = findViewById(R.id.editTextText);
+        EditText editText2 = findViewById(R.id.editTextText2);
+
+        TextView textView = findViewById(R.id.textView);
+        Button button = findViewById(R.id.button2);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text1 = editText1.getText().toString();
+                String text2 = editText2.getText().toString();
+
+                if (!text1.isEmpty() && !text2.isEmpty()) {
+                    try {
+                        int num1 = Integer.parseInt(text1);
+                        int num2 = Integer.parseInt(text2);
+                        int suma = num1 + num2;
+
+                        textView.setText(getString(R.string.text_sum, suma));
+                    } catch (NumberFormatException e) {
+                        textView.setText(getString(R.string.text_error_numbers));
+                    }
+                } else {
+                    textView.setText(getString(R.string.text_error_empty));
+                }
+            }
         });
     }
 }
